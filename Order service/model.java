f0f958ac8eae36c89433df04f86b7ccfa20bdf3a
madhuhitha429 <144253182+madhuhitha429@users.noreply.github.com>
2026-04-04 07:04:03 +1300

    Create model.java

---
+++ Order service/model.java
@@ -0,0 +1,16 @@
+package com.example.orderservice.model;
+
+public class Order {
+    private Long orderId;
+    private Long userId;
+    private Long productId;
+
+    public Long getOrderId() { return orderId; }
+    public void setOrderId(Long orderId) { this.orderId = orderId; }
+
+    public Long getUserId() { return userId; }
+    public void setUserId(Long userId) { this.userId = userId; }
+
+    public Long getProductId() { return productId; }
+    public void setProductId(Long productId) { this.productId = productId; }
+}
